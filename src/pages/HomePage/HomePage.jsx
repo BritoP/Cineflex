7b7bf8136fa37e60a9293bceba9547b97c3da36c
BritoP <britop@alunos.utfpr.edu.br>
2023-05-29 11:20:24 -0300

    HomePage funcional

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import axios from "axios"
+import axios from "axios";
 import { useState } from "react"
 import { useEffect } from "react"
 import { Link } from "react-router-dom"
